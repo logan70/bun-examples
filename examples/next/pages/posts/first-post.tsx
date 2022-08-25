8405045af6b48b2553a47f3b988708c8ee597a88
Logan <logan70@163.com>
2022-08-25 08:54:48 +0800

    feat: next metadata & css

--- examples/next/pages/posts/first-post.tsx
+++ examples/next/pages/posts/first-post.tsx
@@ -1,12 +1,21 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
       </Head>
       <h1>First Post</h1>
       <h2 className={'title'}>
@@ -19,6 +28,6 @@ export default function FirstPost() {
         width={144} // Desired size with correct aspect ratio
         alt="Profile"
       /> */}
-    </>
+    </Layout>
   );
 }
